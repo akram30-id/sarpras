e73d2a74cc046450178cf60831b6972f4e26c02f
akram30-id <afalerin2016@gmail.com>
2024-06-07 08:17:24 +0700

    add: master booking report

--- application/views/area/approve.php
+++ application/views/area/approve.php
@@ -1,3 +1,46 @@
+<div class="row">
+	<div class="col-sm-10">
+		<button class="btn btn-primary mb-4" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+			Cetak Laporan
+		</button>
+		<div class="collapse mb-3" id="collapseExample">
+			<form method="post" action="<?= base_url('area/print_booking') ?>">
+				<div class="row align-items-end">
+					<div class="col-sm-3">
+						<div class="mb-3">
+							<label for="start">Dari Tanggal</label>
+							<input type="date" name="start" id="start" class="form-control">
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="mb-3">
+							<label for="end">Sampai Tanggal</label>
+							<input type="date" name="end" id="end" class="form-control">
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="mb-3">
+							<label for="status">Status Booking</label>
+							<select class="form-select" name="status" aria-label="Default select example">
+								<option disabled>- PILIH STATUS -</option>
+								<?php $status = ['APPROVED', 'PENDING', 'REJECTED'] ?>
+								<?php foreach ($status as $s){ ?>
+									<option value="<?= $s ?>"><?= $s ?></option>
+								<?php } ?>
+								<option value="ALL">ALL</option>
+							</select>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="mb-3">
+							<button type="submit" class="btn btn-primary">Cetak	</button>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
 <div class="row justify-content-center">
 	<div class="col-sm-8">
 		<?php $this->load->view('flashdata'); ?>
