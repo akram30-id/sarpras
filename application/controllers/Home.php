3ee83a2cbdd5a3974609bd740977b1022fb98946
akram30-id <afalerin2016@gmail.com>
2024-06-16 08:18:34 +0700

    add: statistical chart diagram

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -21,6 +21,7 @@ class Home extends CI_Controller
 		$data['total_ekskul'] = $this->_getTotalEkskul();
 		$data['total_pending_area'] = $this->_getTotalSubmissionArea();
 		$data['total_pending_item'] = $this->_getTotalSubmissionItem();
+		$data['statistic'] = base_url('home/get_annual_statistic');
 		$data['content'] = $this->load->view('home', $data, true);
 		$this->load->view('template', $data);
 	}
@@ -108,6 +109,68 @@ class Home extends CI_Controller
 		}
 
 		return $totalSubmission;
+	}
+
+	public function getMonths()
+	{
+		$months = ['JANUARI', 'FEBRUARI', 'MARET', 'APRIL', 'MEI', 'JUNI', 'JULI', 'AGUSTUS', 'SEPTEMBER', 'OKTOBER', 'NOVEMBER', 'DESEMBER'];
+		
+		return $months;
+	}
+
+	private function _getDataAnnual($month)
+	{
+		$year = date('Y');
+		$data = [];
+
+		$getUserAnnual = $this->db->select('COUNT(a.username) AS total_user')
+									->from('tb_user AS a')
+									->where('YEAR(a.created_at) = "' . $year . '"')
+									->where('MONTH(a.created_at) = "' . $month . '"')
+									->get()->row();
+
+		$data['user'] = $getUserAnnual->total_user;
+
+		$getEkskulAnnual = $this->db->select('COUNT(a.ekskul_code) AS total_ekskul')
+									->from('tb_master_ekskul AS a')
+									->where('YEAR(a.created_at) = "' . $year . '"')
+									->where('MONTH(a.created_at) = "' . $month . '"')
+									->get()->row();
+
+		$data['ekskul'] = $getEkskulAnnual->total_ekskul;
+
+		$getItemAnnual = $this->db->select('COUNT(a.submission_item_code) AS total_submission_item')
+									->from('tb_submission_item AS a')
+									->where('YEAR(a.created_at) = "' . $year . '"')
+									->where('MONTH(a.created_at) = "' . $month . '"')
+									->get()->row();
+
+		$data['submission_item'] = $getItemAnnual->total_submission_item;
+
+		$getItemAnnual = $this->db->select('COUNT(a.submission_area_code) AS total_submission_area')
+									->from('tb_submission_area AS a')
+									->where('YEAR(a.created_at) = "' . $year . '"')
+									->where('MONTH(a.created_at) = "' . $month . '"')
+									->get()->row();
+
+		$data['submission_area'] = $getItemAnnual->total_submission_area;
+
+		return $data;
+	}
+
+	public function get_annual_statistic()
+	{
+		$months = $this->getMonths();
+
+		$result = [];
+		foreach ($months as $idxMonth => $month) {
+			$result['user'][] = $this->_getDataAnnual(intval($idxMonth) + 1)['user'];
+			$result['ekskul'][] = $this->_getDataAnnual(intval($idxMonth) + 1)['ekskul'];
+			$result['submission_item'][] = $this->_getDataAnnual(intval($idxMonth) + 1)['submission_item'];
+			$result['submission_area'][] = $this->_getDataAnnual(intval($idxMonth) + 1)['submission_area'];
+		}
+
+		echo json_encode($result);
 	}
 
 }
